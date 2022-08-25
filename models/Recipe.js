2206e94300c3920d179ed452ed203f79ce0347a4
Freddie Yang <dark_yty@hotmail.com>
2022-08-25 20:40:43 +1000

    added models

---
+++ models/Recipe.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model { }
+
+Recipe.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        instruction: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        ingredient_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'ingredient',
+                key: 'id',
+            },
+        },
+        time: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+          },
+        calories: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+          },
+        difficulty: {
+            type: DataTypes.ENUM('Easy', 'Medium', 'Hard'),
+            allowNull: false,
+          },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'recipe',
+    }
+)
+
+module.exports = Recipe;
