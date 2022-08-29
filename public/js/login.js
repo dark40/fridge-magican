da500ed2fe84c1883b4399dd09b6093d46209798
Artur0705 <artlil420@gmail.com>
2022-08-29 21:55:21 +1000

    Added login functionality

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,6 @@ function showPassword() {
   let password = document.getElementById("password");
   let hidePsw = document.getElementById("hidePsw");
   let showPsw = document.getElementById("showPsw");
-
   if (password.type === "password") {
     password.type = "text";
     hidePsw.style.display = "block";
@@ -13,3 +12,28 @@ function showPassword() {
     showPsw.style.display = "block";
   }
 }
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector("#login-now")
+  .addEventListener("click", loginFormHandler);
