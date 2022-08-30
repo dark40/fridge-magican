c4504b30945f2258ea4284a33e7f3aa79fd153ee
Freddie Yang <dark_yty@hotmail.com>
2022-08-30 20:07:58 +1000

    added compare ingredient function

---
+++ utils/helpers.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // recipeIngredientData = [1,2,3,4,5,6,7,8]
+    // fridgeData = [1,3,5]
+    compare_ingredient: (fridgeData, recipeIngredientData) => {
+        let count = 0;
+
+        for (let i = 0; i < fridgeData.length; i++) {
+            for (let j = 0; i < recipeIngredientData.length; j++) {
+                if (fridgeData[i] === recipeIngredientData[j]) {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+
+        return count + "/" + recipeIngredientData.length
+    }
+
+};
