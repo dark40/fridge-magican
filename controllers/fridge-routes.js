cc123e95b6a5b0e1e3c9ae3eb43fde3f815d8bd2
Craig <craig.skicko@gmail.com>
2022-08-31 18:34:30 +1000

    Added functionality to fridge route to pass fridge ingredient data to handlebar

--- controllers/fridge-routes.js
+++ controllers/fridge-routes.js
@@ -4,6 +4,7 @@ const withAuth = require("../utils/auth");
 
 router.get("/:id", withAuth, async (req, res) => {
   try {
+    // Get the fridge data based on the user that's logged in
     const userFridgeData = await User.findByPk(req.params.id, {
       include: [
         {
@@ -12,13 +13,30 @@ router.get("/:id", withAuth, async (req, res) => {
       ]
     });
     
+    // Check that data is returned from the database
     if(!userFridgeData) {
       res.status(404).render("404");
     }
 
+    // Clean the user fridge data
     const userFridgeDataClean = userFridgeData.get({ plain: true });
   
-    res.render("fridge", { userFridgeDataClean , logged_in: req.session.logged_in, current_user: req.session.email, user_id: req.session.user_id });
+    // Get fridge ingredient data
+    const fridgeIngredientData = await Fridge.findByPk(userFridgeDataClean.fridge.id, {
+      include: [
+        {
+          model: Ingredient,
+          through: FridgeIngredient,
+          as: 'stocks'
+        }
+      ]
+    });
+
+    // Clean the fridge ingredient data
+    const fridgeIngredientDataClean = fridgeIngredientData.get({ plain: true });
+
+    // Render the page
+    res.render("fridge", { userFridgeDataClean, fridgeIngredientDataClean, logged_in: req.session.logged_in, current_user: req.session.email, user_id: req.session.user_id });
   } catch (err) {
     res.status(500).json(err);
   }
