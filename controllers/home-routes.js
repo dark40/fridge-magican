6f2b6a6a453aee57142e55248fe7827ed5eec85f
Patrick Gu <patrickthegu@gmail.com>
2022-09-01 20:45:25 +1000

    Home routes working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,25 +1,68 @@
-const { Fridge, Ingredient, Recipe, User } = require("../models");
+const { Fridge, Ingredient, Recipe, User, FridgeIngredient, RecipeIngredient} = require("../models");
 const router = require("express").Router();
+const withAuth = require("../utils/auth");
 
 // Render homepage
-router.get("/", async (req, res) => {
-  Fridge.findAll({
-    attributes: ["id"],
-    include: [
-      {
-        model: User,
-        attributes: ["name", "email"],
+router.get("/", withAuth, async (req, res) => {
+  try{
+    // Get user fridge data
+    const userFridge = await Fridge.findOne({
+      where: {
+        id: req.session.user_id,
       },
-    ],
-  })
-    .then((data) => {
-      const posts = data.map((post) => post.get({ plain: true }));
-      res.render("home", { posts, logged_in: req.session.logged_in, current_user: req.session.email, user_id: req.session.user_id });
-    })
-    .catch((err) => {
-      res.status(500).json(err);
+      attributes: ["id", "user_id"],
+      include: [
+        {
+          model: User,
+          attributes: ["name", "email"],
+        },
+        {
+          model: Ingredient,
+          through: FridgeIngredient,
+          as: "stocks",
+          attributes: ["id", "name"],
+        },
+      ],
     });
+    if(!userFridge){
+      res.status(404).render("404");
+    }
+
+    // Clean userFridge data
+    const userFridgeClean = userFridge.get({plain: true});
+    console.log(userFridgeClean);
+
+    // Get All recipes
+    const userRecipe = await Recipe.findAll({
+      attributes: [
+        "id",
+        "name",
+        "time",
+        "calories",
+        "difficulty",
+      ],
+      include: [
+        {
+          model: Ingredient,
+          through: RecipeIngredient,
+          as: "ingredients",
+          attributes: ["id", "name"],
+        },
+      ],
+    })
+
+    // Clean Recipe data
+    const userRecipeClean = userRecipe.map ((userRecipe) => userRecipe.get({plain: true}));
+    console.log(userRecipeClean);
+
+    // Render home page
+    res.render("home", { userFridgeClean, userRecipeClean, logged_in: req.session.logged_in, current_user: req.session.email, user_id: req.session.user_id });   
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
+
+  
 
 // Render login page
 router.get("/login", async (req, res) => {
