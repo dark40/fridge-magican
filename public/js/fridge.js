bd508259f105d376338ecfe53466dbd6702d3b50
Freddie Yang <dark_yty@hotmail.com>
2022-09-01 20:10:35 +1000

    added draft fridge function

--- public/js/fridge.js
+++ public/js/fridge.js
@@ -1,58 +1,40 @@
-// const saveBtn = document.querySelector(".save");
-// const ingredient = document.querySelector(".ingredient-list");
-// const stock = document.querySelector(".stock-list");
-// const AllIngredients = require("../../seeds/ingredientData.json");
-// const fridgeIngredient = require("../../seeds/fridgeIngredientData.json");
-// const fs = require("fs");
+const saveBtn = document.querySelector(".save");
+const ingredient = document.querySelector(".ingredient-list");
+const stock = document.querySelector(".stock-list");
 
-// const AllIngredientsData = JSON.parse(AllIngredients).name;
+let activeIngredient = {};
 
-// const handleSave = (event) => {
-//     event.preventDefault();
+const saveIngredient = (ingredient) => 
+    fetch('/api/fridge/:id', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(ingredient),
+    });
 
-    // add render 
-// }
+    // fridge_id, ingredient_id; 
 
-// const addIngredient = (event) => {
-//     event.preventDefault();
+const addIngredient = (event) => {
+    event.preventDefault();
 
-//     const targetIngredient = event.target;
-//     const ingredientName = JSON.parse(targetIngredient.parentElement.getAttribute('name'))
+    const targetIngredient = event.target;
 
-//     // Check if ingredient is already in the list. If not, add to stocks.
-//     for (let i = 0; i < AllIngredientsData.length; i++) {
-//         if (ingredientName === AllIngredientsData[i]) {
-//             break;
-//         } else {
-//             let fridge_id = document.location.search;
-//             let ingredient_id = AllIngredients[i].id;
+    // Check if ingredient is already in the list. If not, add to stocks.
+    for (let i = 0; i < AllIngredientsData.length; i++) {
+        if (ingredientName === AllIngredientsData[i]) {
+            break;
+        } else {
+            let fridge_id = document.location.search;
+            let ingredient_id = AllIngredients[i].id;
 
-//             fs.readFile(fridgeIngredient, 'utf8', function readFileCallback(err, data) {
-//                 if (err) {
-//                     console.log(err);
-//                 } else {
-//                     let obj = {
-//                         table: []
-//                     };
-//                     obj = JSON.parse(data); //now it an object
-//                     obj.table.push({ fridge_id: fridge_id, ingredient_id: ingredient_id }); //add some data
-//                     json = JSON.stringify(obj); //convert it back to json
-//                     fs.writeFile(fridgeIngredient, json, 'utf8', callback); // write it back 
-//                 }
-//             });
-//         }
-//     }
-// }
-
-// const removeIngredient = (event) => {
-//     event.preventDefault();
+                }
+            });
+        }
+    }
+}
 
 
-
-// }
-
-
-
-// saveBtn.addEventListener("click", handleSave);
+saveBtn.addEventListener("click", );
 // ingredient.addEventListener("click", addIngredient);
 // stock.addEventListener("click", removeIngredient);
